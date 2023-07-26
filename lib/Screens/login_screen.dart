b31cb6de6cec59489916fb119ba1b129977137e5
Hapopa <82116969+Hapopa@users.noreply.github.com>
2023-07-26 12:20:06 +0300

    add localization files and apply it to login screen

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:kashkool/Style/my_theme.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LoginScreen extends StatelessWidget {
 static const String routeName="LoginScreen";
@@ -21,7 +22,7 @@ GlobalKey formKey=GlobalKey<FormState>();
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: MyTheme.bg,
-                  label: Text('User Name',style: TextStyle(color: Colors.grey),),
+                  label: Text(AppLocalizations.of(context)?.user_name??"jjjj",style: TextStyle(color: Colors.grey),),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
                     borderSide: BorderSide(color: Colors.grey)
@@ -38,7 +39,7 @@ GlobalKey formKey=GlobalKey<FormState>();
                 decoration: InputDecoration(
                     filled: true,
                     fillColor: MyTheme.bg,
-                    label: Text('Password',style: TextStyle(color: Colors.grey),),
+                    label: Text(AppLocalizations.of(context)!.password,style: TextStyle(color: Colors.grey),),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(12),
                         borderSide: BorderSide(color: Colors.grey)
@@ -52,18 +53,18 @@ GlobalKey formKey=GlobalKey<FormState>();
               SizedBox(height: 10,),
               Container(
                 width: double.infinity,
-                child: ElevatedButton(onPressed: (){}, child: Text('Log in'),
+                child: ElevatedButton(onPressed: (){}, child: Text(AppLocalizations.of(context)!.log_in),
                   style: ElevatedButton.styleFrom(
                     primary: MyTheme.blue
                   )
                   ),
                 ),
               SizedBox(height: 10,),
-              TextButton(onPressed: (){}, child: Text('Forgot password?',style: TextStyle(color: Colors.black),)),
+              TextButton(onPressed: (){}, child: Text(AppLocalizations.of(context)!.forgot_password,style: TextStyle(color: Colors.black),)),
               Spacer(),
               Container(
                   width: double.infinity,
-                  child: ElevatedButton(onPressed: (){}, child: Text('Create new account',style: TextStyle(color: MyTheme.blue),),
+                  child: ElevatedButton(onPressed: (){}, child: Text(AppLocalizations.of(context)!.create_account,style: TextStyle(color: MyTheme.blue),),
                   style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(
                       side: BorderSide(color: MyTheme.blue),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:kashkool/Layout/home_screen.dart';
 import 'package:kashkool/Screens/login_screen.dart';
 import 'package:kashkool/Style/my_theme.dart';
@@ -20,6 +22,17 @@ class MyApp extends StatelessWidget {
         HomeScreen.routeName:(_)=>HomeScreen(),
         LoginScreen.routeName:(_)=>LoginScreen(),
       },
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('ar'), // Spanish
+      ],
+      locale: Locale('ar'),
       theme: MyTheme.lightMode,
       darkTheme: MyTheme.darkMode,
     );
